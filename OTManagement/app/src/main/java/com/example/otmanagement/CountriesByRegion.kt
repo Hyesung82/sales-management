670f8eef9d4eac4b7231713a4b0ca8dad1a9c3e0
Hyesung <comet_0419@naver.com>
2021-06-08 03:02:43 +0900

    Add google map

--- OTManagement/app/src/main/java/com/example/otmanagement/CountriesByRegion.kt
+++ OTManagement/app/src/main/java/com/example/otmanagement/CountriesByRegion.kt
@@ -1,12 +1,24 @@
 package com.example.otmanagement
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.otmanagement.databinding.ActivityCountriesByRegionBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class CountriesByRegion : AppCompatActivity() {
+class CountriesByRegion : AppCompatActivity(), AdapterView.OnItemSelectedListener, OnMapReadyCallback {
+    val TAG = "CountriesByRegion"
     private lateinit var binding: ActivityCountriesByRegionBinding
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +27,49 @@ class CountriesByRegion : AppCompatActivity() {
         binding.ivBack.setOnClickListener {
             onBackPressed()
         }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.regions_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            binding.sRegion.adapter = adapter
+        }
+
+        binding.sRegion.onItemSelectedListener = this
+
+
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.fMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+
+        binding.sRegion.setSelection(intent.getIntExtra(EXTRA_REGION, 0))
+        Log.d(TAG, "extra: ${intent.getIntExtra(EXTRA_REGION, 0)}")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val seoul = LatLng(37.56, 126.97)
+
+        val markerOptions = MarkerOptions()
+        markerOptions.position(seoul)
+        markerOptions.title("서울")
+        markerOptions.snippet("한국의 수도")
+        mMap.addMarker(markerOptions)
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10F))
     }
 }
--- OTManagement/app/src/main/java/com/example/otmanagement/MainActivity.kt
+++ OTManagement/app/src/main/java/com/example/otmanagement/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.otmanagement
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.otmanagement.databinding.ActivityMainBinding
 
+
+const val EXTRA_REGION = "region"
+
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
     private lateinit var binding: ActivityMainBinding
+    private var selectedItem: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +34,26 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.bCheck.setOnClickListener {
-            startActivity(Intent(this, CountriesByRegion::class.java))
+            val intent = Intent(this, CountriesByRegion::class.java).apply {
+                putExtra(EXTRA_REGION, selectedItem)
+            }
+            Log.d(TAG, "region num: $selectedItem")
+            startActivity(intent)
+        }
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line, REGIONS
+        )
+        binding.actvRegion.setAdapter(adapter)
+
+        binding.actvRegion.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id->
+//            val selectedItem = parent.getItemAtPosition(position).toString()
+            selectedItem = position
         }
     }
+
+    private val REGIONS = arrayOf(
+        "Europe", "Americas", "Asia", "Middle East and Africa"
+    )
 }
